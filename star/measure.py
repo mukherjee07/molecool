02184914cce0f462b073d0bb48ad4a11f33db0bc
Krishnendu Mukherjee <kmukherj@buffalo.edu>
2020-12-17 13:56:39 -0500

    doc-string-add

--- star/measure.py
+++ star/measure.py
@@ -20,6 +20,28 @@ def calculate_angle(rA, rB, rC, degrees=False):
         return theta
 
 def calculate_distance(rA, rB):
+    """
+    Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA : np.ndarray
+	The coordinate of point A.    	
+    rB : np.ndarray
+	The coordinate of point B.
+
+    Returns
+    -------
+    dist : float
+	The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0,0,0])
+    >>> r2 = np.array([0,1,0])
+    >>> calculate_distance(r1,r2)
+
+    """
     # This function calculates the distance between two points given as numpy arrays.
     d=(rA-rB)
     dist=np.linalg.norm(d)
